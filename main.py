666f01a17496b9c5742889be68923185864b1189
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-22 00:28:49 +0100

    update main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 from db import Database, iterate_collection
-from proxy_rotator import ProxyRotator, get_proxies
-from amazon import get_amazon_html, parse_amazon_product
-
+from proxies import ProxyRotator, get_proxies
+from amazon import get_amazon_price
+from tradeinn import get_tradeinn_price
 
 AMAZON_ID = "678fe61421cc010007e27780"
-
+TRADEINN_ID = "67900fe721cc010007e27784"
 
 def main():
     load_dotenv()
@@ -16,14 +16,20 @@ def main():
     proxy_rotator = ProxyRotator(proxies)
 
     for product in iterate_collection(db, "monitored"):
+        
+        product_id = str(product["product_id"])
         retailer_id = str(product["retailer_id"])
+        country_id = str(product["country_id"])
         url = product["url"]
+
         if retailer_id == AMAZON_ID:
-            html = get_amazon_html(proxy_rotator, url)
-            product = parse_amazon_product(html)
-            print(product)
+            price = get_amazon_price(proxy_rotator, country_id, url)
+        elif retailer_id == TRADEINN_ID:
+            price = get_tradeinn_price(proxy_rotator, country_id, url)
         else:
             print(f"Unknown retailer: {product}")
+
+        print(f"{product_id} {price}") # store in db
 
 
 if __name__ == "__main__":
