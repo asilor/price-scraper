3c981cb78cedf54ec3497896db51fd5a03afb6bd
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-21 22:40:01 +0100

    scrape amazon

---
+++ amazon.py
@@ -0,0 +1,32 @@
+from proxy_rotator import ProxyRotator
+from selectolax.parser import HTMLParser
+
+
+def get_amazon_html(proxy_rotator: ProxyRotator, url: str) -> str:
+    """Given an ASIN, scrape and save the product information."""
+    
+    while True:
+        html = proxy_rotator.get_content(url)
+        if "Enter the characters you see below" not in html:
+            print(f"{url} Success")
+            return html
+        else:
+            print(f"{url} CAPTCHA")
+
+
+def parse_amazon_product(html: str) -> str:
+    """Given the HTML, return the product information."""
+
+    tree = HTMLParser(html)
+
+    title_element = tree.css_first("h1 span")
+    price_symbol_element = tree.css_first("span.a-price-symbol")
+    price_whole_element = tree.css_first("span.a-price-whole")
+    price_fraction_element = tree.css_first("span.a-price-fraction")
+
+    product_title = title_element.text().strip() if title_element else "Title not found"
+    price_symbol = price_symbol_element.text() if price_symbol_element else "Symbol not found"
+    price_whole = price_whole_element.text().replace(".", "") if price_whole_element else "Whole part not found"
+    price_fraction = price_fraction_element.text() if price_fraction_element else "Fraction not found"
+
+    return f"{product_title} {price_symbol}{price_whole}.{price_fraction}"
