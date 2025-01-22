9ca4ddee618816b47ccf65adf8f3acb026cba348
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-23 00:03:52 +0100

    paralelize requests with thread pool executer

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, UTC
 from dotenv import load_dotenv
+from bson.objectid import ObjectId
 from db import Database, iterate_collection
 from proxies import ProxyRotator, get_proxies
 from amazon import get_amazon_price
 from tradeinn import get_tradeinn_price
-from datetime import datetime, UTC
-from bson.objectid import ObjectId
 
 
 AMAZON_ID = "678fe61421cc010007e27780"
 TRADEINN_ID = "67900fe721cc010007e27784"
 
 
-def store_price(db, product_id: str, region_id: str, retailer_id: str, price: float):
-    """Store the price in the database."""
+def process_product(db, proxy_rotator, product):
+    product_id = str(product["product_id"])
+    retailer_id = str(product["retailer_id"])
+    country_id = str(product["country_id"])
+    url = product["url"]
+
+    if retailer_id == AMAZON_ID:
+        price = get_amazon_price(proxy_rotator, country_id, url)
+    elif retailer_id == TRADEINN_ID:
+        price = get_tradeinn_price(proxy_rotator, country_id, url)
+    else:
+        print(f"Unknown retailer: {product}")
 
     price_document = {
         "product_id": ObjectId(product_id),
         "retailer_id": ObjectId(retailer_id),
-        "country_id": ObjectId(region_id),
+        "country_id": ObjectId(country_id),
         "price": price,
         "time_checked": datetime.now(UTC)
     }
@@ -26,29 +37,18 @@ def store_price(db, product_id: str, region_id: str, retailer_id: str, price: fl
     prices_collection.insert_one(price_document)
 
 
-def main():
+def main() -> None:
     load_dotenv()
- 
+
     db = Database()
 
     proxies = get_proxies()
     proxy_rotator = ProxyRotator(proxies)
 
-    for product in iterate_collection(db, "monitored"):
-        
-        product_id = str(product["product_id"])
-        retailer_id = str(product["retailer_id"])
-        country_id = str(product["country_id"])
-        url = product["url"]
-
-        if retailer_id == AMAZON_ID:
-            price = get_amazon_price(proxy_rotator, country_id, url)
-        elif retailer_id == TRADEINN_ID:
-            price = get_tradeinn_price(proxy_rotator, country_id, url)
-        else:
-            print(f"Unknown retailer: {product}")
-
-        store_price(db, product_id, country_id, retailer_id, price)
+    NUM_WORKERS = 50
+    with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
+        for product in iterate_collection(db, "monitored"):
+            executor.submit(process_product, db, proxy_rotator, product)
 
 
 if __name__ == "__main__":
