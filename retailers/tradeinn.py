edb2b4edba5b48c46aa44aa9bcd1af70954d245b
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-24 15:32:49 +0100

    tradeinn scraper for any product for any country

--- retailers/tradeinn.py
+++ retailers/tradeinn.py
@@ -1,19 +1,43 @@
 from proxies import ProxyRotator
-from selectolax.parser import HTMLParser
+import json
 
 
 TRADEINN_ID = "67900fe721cc010007e27784"
 
 
 def get_tradeinn_price(proxy_rotator: ProxyRotator, country_id: str, url: str):
-    html = proxy_rotator.get_content(url)
+    id_modelo = url.split("/")[-2]
+    id_pais = get_tradeinn_id_pais(country_id)
+    url_elastic_dc = f"https://dc.tradeinn.com/{id_modelo}"
+
+    headers = {
+        'referer': 'https://www.tradeinn.com/'
+    }
     
-    tree = HTMLParser(html)
+    response = proxy_rotator.get_content(url_elastic_dc, headers=headers)
+    product_json = json.loads(response)
+    
+    product = product_json["_source"]["productes"][0]
 
-    price_element = tree.css_first("p#js-precio")
-    price_text = price_element.text().strip()
-    price = float(price_text.split()[0])
-
-    print(f"{url} {price}")
+    price = None
+    for seller in product["sellers"]:
+        for precio_pais in seller["precios_paises"]:
+            if precio_pais["id_pais"] == id_pais:
+                price = precio_pais["precio"]
+                break
+        if price is not None:
+            break
 
     return price
+
+
+def get_tradeinn_id_pais(country_id: str) -> int:
+    """Returns the Tradeinn ID for the given country ID."""
+
+    country_id_to_id_pais = {
+        "678fe4be21cc010007e2777e": 180,
+        "67939fcb21cc010007e27790": 70,
+        "6793a19121cc010007e27792": 75,
+    }
+    
+    return country_id_to_id_pais.get(country_id, 180)
