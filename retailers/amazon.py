bc4c4b0f96892d6456bf064a0487ee8e206bc8e4
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-25 03:23:05 +0100

    scrape amazon product

--- retailers/amazon.py
+++ retailers/amazon.py
@@ -1,4 +1,5 @@
 from database import Database, store_price
+from bson.objectid import ObjectId
 from proxies import ProxyRotator
 from selectolax.parser import HTMLParser
 
@@ -7,10 +8,16 @@ AMAZON_ID = "678fe61421cc010007e27780"
 
 
 def get_amazon_price(db: Database, proxy_rotator: ProxyRotator, product: dict) -> None:
-    # html = get_amazon_html(proxy_rotator, url)
-    # price = parse_amazon_product(html)
-    price = 809.33
-    return price
+    """Gets the price of the given product and stores it in the database."""
+
+    url = str(product["url"])
+    html = get_amazon_html(proxy_rotator, url)
+    price = parse_amazon_price(html)
+
+    product["retailer_id"] = ObjectId(AMAZON_ID)
+
+    store_price(db, product, price)
+    print(f"url: {url}, price: {price}")
 
 
 def get_amazon_html(proxy_rotator: ProxyRotator, url: str) -> str:
@@ -25,19 +32,17 @@ def get_amazon_html(proxy_rotator: ProxyRotator, url: str) -> str:
             print(f"{url} CAPTCHA")
 
 
-def parse_amazon_product(html: str) -> str:
-    """Given the HTML, return the product information."""
+def parse_amazon_price(html: str) -> float:
+    """Given the Amazon product page HTML, return the price."""
 
     tree = HTMLParser(html)
 
-    title_element = tree.css_first("h1 span")
-    price_symbol_element = tree.css_first("span.a-price-symbol")
     price_whole_element = tree.css_first("span.a-price-whole")
     price_fraction_element = tree.css_first("span.a-price-fraction")
 
-    product_title = title_element.text().strip() if title_element else "Title not found"
-    price_symbol = price_symbol_element.text() if price_symbol_element else "Symbol not found"
-    price_whole = price_whole_element.text().replace(".", "") if price_whole_element else "Whole part not found"
-    price_fraction = price_fraction_element.text() if price_fraction_element else "Fraction not found"
+    price_whole = price_whole_element.text().replace(".", "").replace(",", "")
+    price_fraction = price_fraction_element.text()
 
-    return f"{product_title} {price_symbol}{price_whole}.{price_fraction}"
+    price = f"{price_whole}.{price_fraction}"
+
+    return float(price)
